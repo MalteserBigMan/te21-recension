a66af38de4b29f326a34ae66ad567fb6c9c26926
malte <malte.lindkvist@elev.ga.ntig.se>
2024-02-08 10:20:12 +0100

    Lektion

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const router = express.Router()
+const pool = require('../db')
 
-router.get('/', function (req, res) {
-  res.render('index.njk', { title: 'Welcome' })
+router.get('/:id', async (req, res) => {
+    try {
+        const id = parseInt(req.params.id)
+      const [part] = await pool.promise().query('SELECT * FROM malte_part where id = ' + id)
+      res.render('index.njk', { title: part[0].text, href:   "http://localhost:3000/" + (id + 1) })  
+    } catch (error) {
+        console.log(error)
+    }
+
 })
+
 
 module.exports = router
