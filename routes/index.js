40744cd0ee9a87f44e6906cdf140645d3495dbc6
malte <malte.lindkvist@elev.ga.ntig.se>
2024-02-08 11:13:09 +0100

    lektion

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,30 @@ const express = require('express')
 const router = express.Router()
 const pool = require('../db')
 
-router.get('/:id', async (req, res) => {
+router.get('/', async (req, res) => {
     try {
         const id = parseInt(req.params.id)
-      const [part] = await pool.promise().query('SELECT * FROM malte_part where id = ' + id)
-      res.render('index.njk', { title: part[0].text, href:   "http://localhost:3000/" + (id + 1) })  
+    //   const [part] = await pool.promise().query('SELECT * FROM malte_part where id = ' + id)
+
+    const movies = [
+        {
+            "title": "Ursus",
+            "score": "4.5⭐"
+        },
+        {
+            "title": "Malets äventyr",
+            "score": "9.7⭐"
+        },
+        {
+            "title": "Keros Kast",
+            "score": "9.2⭐"
+        },
+        {
+            "title": "Samuels Sugeri",
+            "score": "10⭐"
+        }
+    ]
+      res.render('index.njk', { title: "MMDB", movies})  
     } catch (error) {
         console.log(error)
     }
